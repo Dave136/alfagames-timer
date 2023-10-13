83e3e94147438320c8341676196d0ca7a13228eb
David Arenas <davejs136@gmail.com>
2023-10-13 19:13:50 -0400

    feat: add basic config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/ui'
+  ],
+  ui: {
+    global: true,
+    icons: ['ph', 'heroicons']
+  },
+  colorMode: {
+    preference: 'dark'
+  }
 })
