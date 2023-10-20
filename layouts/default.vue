d4f7a1ce2dccb184f7938fb184c744aa2e53c34f
David Arenas <davejs136@gmail.com>
2023-10-19 22:10:11 -0400

    feat: show resume when is home

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="grid grid-cols-[5rem_1fr_25rem] w-full">
+  <div class="grid w-full min-h-screen"
+    :class="`${$route.path === '/' ? 'grid-cols-[5rem_1fr_25rem]' : 'grid-cols-[5rem_1fr]'}`">
     <AppAside />
     <slot />
-    <AppResume />
+    <AppResume v-if="$route.path === '/'" />
   </div>
 </template>
