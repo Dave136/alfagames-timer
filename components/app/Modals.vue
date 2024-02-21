c16515b9d655cb2cc6f5075aee1a8372405cf65c
David Arenas <davejs136@gmail.com>
2024-02-20 22:47:39 -0400

    chore: bump v0.2.0

--- components/app/Modals.vue
+++ components/app/Modals.vue
@@ -14,7 +14,7 @@ const settings = toRef(appStore.modals, 'settings')
       <div class="p-6">
         <p><b>Autor:</b> David Arenas</p>
         <p><b>Licencia:</b> MIT</p>
-        <p><b>Version:</b> 0.1.7</p>
+        <p><b>Version:</b> 0.2.0</p>
         <p><b>Website:</b> <a href="https://github.com/Dave136/alfagames-timer" target="_blank"
             class="underline">alfagames timer</a></p>
       </div>
